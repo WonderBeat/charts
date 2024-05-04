17116e7f85bbb0b331396712d1c99b4ba914fecc
WonderBeat <borov.htid@gmail.com>
2024-05-04 18:11:09 +0300

    dapr pubsub for liquid8

--- infra/inventory.py
+++ infra/inventory.py
@@ -6,7 +6,7 @@ oracle_denis = [
     ("kesaram", {"k8s_master": True, "multicloud_gateway": True}),
 ]
 
-oracle_andrey = ["o1", "o2", "o3", "o4"]
+oracle_andrey = ["o1", "o2", "o3"]
 hezner_eu = ["ks2"]
 hezner_us = ["usa1"]
 
