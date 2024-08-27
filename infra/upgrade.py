1a57efa2c5069370ac9484b1b7aa080cc0b409b0
WonderBeat <borov.htid@gmail.com>
2024-08-27 22:07:24 +0300

    vacuum journal

--- infra/upgrade.py
+++ infra/upgrade.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 from pyinfra.operations import apt, files, server
 
-apt.update(name="Update apt repositories", cache_time=3600, _sudo=True, _parallel=True)
+apt.update(name="Update apt repositories", cache_time=3600, _sudo=True)
 
 apt.upgrade(_sudo=True, _parallel=True)
+
+server.shell(name="vacuum", commands=["journalctl --vacuum-time=2d"], _sudo=True)
